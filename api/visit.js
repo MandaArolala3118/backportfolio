b56bd1bc4103f5e7bc50a54388e0f3cbb081d9bf
Manda Arolala ANDRIANINA <mandaarolalaa@gmail.com>
2026-03-30 16:29:23 +0300

    test encore 3

--- api/visit.js
+++ api/visit.js
@@ -2,9 +2,12 @@ module.exports = async (req, res) => {
   const origin = req.headers.origin;
   const allowedOrigin = process.env.ALLOWED_ORIGIN;
 
-  if (origin === allowedOrigin) {
-    res.setHeader('Access-Control-Allow-Origin', origin);
-  }
+  console.log('METHOD:', req.method);
+  console.log('origin reçu:', origin);
+  console.log('ALLOWED_ORIGIN env:', allowedOrigin);
+  console.log('match:', origin === allowedOrigin);
+
+  res.setHeader('Access-Control-Allow-Origin', 'https://portfolio-green-psi-38.vercel.app');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Credentials', 'true');
