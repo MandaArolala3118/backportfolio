2d2a86a1f232d9af27f8f1fce780e05e8a9ee839
Manda Arolala ANDRIANINA <mandaarolalaa@gmail.com>
2026-03-30 16:25:48 +0300

    test  encore 2

--- api/visit.js
+++ api/visit.js
@@ -1,3 +1,17 @@
 module.exports = async (req, res) => {
+  const origin = req.headers.origin;
+  const allowedOrigin = process.env.ALLOWED_ORIGIN;
+
+  if (origin === allowedOrigin) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+
   res.status(200).json({ ok: true });
 };
